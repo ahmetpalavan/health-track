563f9c65d30d3f47386747ec93634e302637ed77
Ahmet Palavan <ahmetpalavann@gmail.com>
2024-08-22 16:05:03 +0300

    feat: Added PasskeyModal component to Home page and updated the displayed company name.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { PatientForm } from '~/components/forms';
+import { PasskeyModal } from '~/components/passkey-modal';
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams?.admin === 'true';
   return (
     <div className='flex h-screen max-h-screen'>
+      {isAdmin && <PasskeyModal />}
       <section className='remove-scrollbar container my-auto'>
         <div className='sub-container max-w-[496px]'>
           <Image src='/icons/logo-full.svg' height={1000} width={1000} alt='patient' className='mb-12 h-10 w-fit' />
@@ -12,7 +15,7 @@ export default function Home() {
           <PatientForm />
 
           <div className='text-14-regular mt-20 flex justify-between'>
-            <p className='justify-items-end text-dark-600 xl:text-left'>© 2024 CarePluse</p>
+            <p className='justify-items-end text-dark-600 xl:text-left'>© 2024 HealthTrack</p>
             <Link href='/?admin=true' className='text-green-500'>
               Admin
             </Link>
